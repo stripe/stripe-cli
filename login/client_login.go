465acefa41733968709dab730dcc7beb5cdb932b
jmuia-stripe <52180152+jmuia-stripe@users.noreply.github.com>
2019-07-03 18:01:37 -0700

    Implement getLinks() to kick off the login flow (#6)
    * Implement getLinks() to kick off the login flow
    
    * Replace raw http client with stripe http client and use cleaner Postform API
    
    * Return Links pointer

---
+++ login/client_login.go
@@ -0,0 +1,54 @@
+package login
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stripe/stripe-cli/stripeauth"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const stripeCLIAuthURL = "https://dashboard.stripe.com/stripecli/auth"
+
+// Links provides the URLs for the CLI to continue the login flow
+type Links struct {
+	BrowserURL       string `json:"browser_url"`
+	PollURL          string `json:"poll_url"`
+	VerificationCode string `json:"verification_code"`
+}
+
+func getLinks(authURL string, deviceName string) (*Links, error) {
+	client := stripeauth.NewHTTPClient("")
+
+	if authURL == "" {
+		authURL = stripeCLIAuthURL
+	}
+
+	data := url.Values{}
+	data.Set("device_name", deviceName)
+
+	res, err := client.PostForm(authURL, data)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected http status code: %d %s", res.StatusCode, string(bodyBytes))
+	}
+
+	var links Links
+	err = json.Unmarshal(bodyBytes, &links)
+	if err != nil {
+		return nil, err
+	}
+
+	return &links, nil
+}
