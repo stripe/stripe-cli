bd0c791a71a1dcfc965c2186d2d980de27fd807d
tomer-stripe <42354557+tomer-stripe@users.noreply.github.com>
2019-07-11 14:45:03 -0700

    Add feedback command (#20)
    * Add feedback command
    
    * Update .goreleaser.yml (#16)
    
    * Use a shorter stripe

---
+++ pkg/cmd/feedback.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+
+	"github.com/stripe/stripe-cli/pkg/validators"
+)
+
+type feedbackCmd struct {
+	cmd *cobra.Command
+}
+
+func newFeedbackdCmd() *feedbackCmd {
+	return &feedbackCmd{
+		cmd: &cobra.Command{
+			Use:   "feedback",
+			Args:  validators.NoArgs,
+			Short: "Provide us with feedback on the CLI",
+			Run: func(cmd *cobra.Command, args []string) {
+				os := getOS()
+				url := fmt.Sprintf("https://stri.pe/cli-feedback%s", os)
+
+				output := `
+     _        _
+ ___| |_ _ __(_)_ __   ___
+/ __| __| '__| | '_ \ / _ \
+\__ \ |_| |  | | |_) |  __/
+|___/\__|_|  |_| .__/ \___|
+               |_|
+
+We'd love to know what you think of the CLI:
+
+* Report bugs or issues on GitHub: https://github.com/stripe/stripe-cli/issues
+* Leave us feedback on how you're using it or features you'd like to see: %s
+				`
+
+				fmt.Println(fmt.Sprintf(output, url))
+			},
+		},
+	}
+}
+
+func getOS() string {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		return "?os=Mac"
+	case "linux":
+		return "?os=Linux"
+	case "windows":
+		return "?os=Windows"
+	default:
+		return ""
+	}
+}
