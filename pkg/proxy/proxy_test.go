c0f0c148b7bf78bac2a447d7b2a380cb6e7de3ba
Olivier Bellone <ob@stripe.com>
2019-07-17 10:37:57 -0700

    Truncate body and headers in webhook responses (#34)

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -31,3 +31,13 @@ func TestFilterWebhookEvent(t *testing.T) {
 	assert.True(t, proxyUseLatest.filterWebhookEvent(evtDefault))
 	assert.False(t, proxyUseLatest.filterWebhookEvent(evtLatest))
 }
+
+func TestTruncate(t *testing.T) {
+	assert.Equal(t, "Hello, World", truncate("Hello, World", 12, false))
+	assert.Equal(t, "Hello, Worl", truncate("Hello, World", 11, false))
+	assert.Equal(t, "Hello, W...", truncate("Hello, World", 11, true))
+
+	assert.Equal(t, "Hello, 世界", truncate("Hello, 世界", 13, false))
+	assert.Equal(t, "Hello, 世", truncate("Hello, 世界", 12, false))
+	assert.Equal(t, "Hello, ...", truncate("Hello, 世界", 12, true))
+}
