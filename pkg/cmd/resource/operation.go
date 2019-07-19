9069190fc1f084e2845896a7862ca36587a3beb3
Olivier Bellone <ob@stripe.com>
2019-07-19 13:33:36 -0700

    Add structs for namespace, resource and operation commands

---
+++ pkg/cmd/resource/operation.go
@@ -0,0 +1,143 @@
+package resource
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/stripe/stripe-cli/pkg/ansi"
+	"github.com/stripe/stripe-cli/pkg/requests"
+)
+
+//
+// Public types
+//
+
+// OperationCmd represents operation commands. Operation commands are nested
+// under resource commands and represent a specific API operation for that
+// resource.
+//
+// Examples of operations: `create`, `retrieve` (standard CRUD methods),
+// `capture` (custom method for the `charges` resource).
+type OperationCmd struct {
+	*requests.Base
+
+	Name      string
+	HTTPVerb  string
+	Path      string
+	URLParams []string
+}
+
+func (oc *OperationCmd) runOperationCmd(cmd *cobra.Command, args []string) error {
+	secretKey, err := oc.Profile.GetSecretKey()
+	if err != nil {
+		return err
+	}
+
+	path := formatURL(oc.Path, args[:len(oc.URLParams)])
+
+	_, err = oc.MakeRequest(secretKey, path, &oc.Parameters)
+
+	return err
+}
+
+//
+// Public functions
+//
+
+// NewOperationCmd returns a new OperationCmd.
+func NewOperationCmd(parentCmd *cobra.Command, name, path, httpVerb string) *OperationCmd {
+	urlParams := extractURLParams(path)
+	httpVerb = strings.ToUpper(httpVerb)
+	operationCmd := &OperationCmd{
+		Base: &requests.Base{
+			Method: httpVerb,
+		},
+		Name:      name,
+		HTTPVerb:  httpVerb,
+		Path:      path,
+		URLParams: urlParams,
+	}
+	cmd := &cobra.Command{
+		Use:         name,
+		Annotations: make(map[string]string),
+		RunE:        operationCmd.runOperationCmd,
+		Args:        cobra.MinimumNArgs(len(urlParams)),
+	}
+	cmd.SetUsageTemplate(operationUsageTemplate(urlParams))
+	cmd.DisableFlagsInUseLine = true
+	operationCmd.Cmd = cmd
+	operationCmd.InitFlags()
+
+	parentCmd.AddCommand(cmd)
+	parentCmd.Annotations[name] = "operation"
+
+	return operationCmd
+}
+
+//
+// Private functions
+//
+
+func extractURLParams(path string) []string {
+	re := regexp.MustCompile(`{\w+}`)
+	return re.FindAllString(path, -1)
+}
+
+func formatURL(path string, urlParams []string) string {
+	s := make([]interface{}, len(urlParams))
+	for i, v := range urlParams {
+		s[i] = v
+	}
+	re := regexp.MustCompile(`{\w+}`)
+	format := re.ReplaceAllString(path, "%s")
+	return fmt.Sprintf(format, s...)
+}
+
+func operationUsageTemplate(urlParams []string) string {
+	args := strings.Map(func(r rune) rune {
+		switch r {
+		case '{':
+			return '<'
+		case '}':
+			return '>'
+		}
+		return r
+	}, strings.Join(urlParams, " "))
+
+	return fmt.Sprintf(`%s{{if .Runnable}}
+  {{.UseLine}} %s{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+%s
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+%s
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+%s{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+%s
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+%s
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+%s{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`,
+		ansi.Bold("Usage:"),
+		args,
+		ansi.Bold("Aliases:"),
+		ansi.Bold("Examples:"),
+		ansi.Bold("Available Operations:"),
+		ansi.Bold("Flags:"),
+		ansi.Bold("Global Flags:"),
+		ansi.Bold("Additional help topics:"),
+	)
+}
