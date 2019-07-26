5163b73fc1ae8aa8aea5ded777d7861d024c0645
Aaron Green <aarongreen@stripe.com>
2019-07-26 14:06:28 -0700

    Add websocket test for request log events

--- pkg/websocket/client_test.go
+++ pkg/websocket/client_test.go
@@ -79,3 +79,67 @@ func TestClientWebhookEventHandler(t *testing.T) {
 	assert.Equal(t, "t=123,v1=hunter2", rcvMsg.HTTPHeaders["Stripe-Signature"])
 	assert.Equal(t, "{}", rcvMsg.EventPayload)
 }
+
+func TestClientRequestLogEventHandler(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	upgrader := ws.Upgrader{}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.NotEmpty(t, r.UserAgent())
+		assert.NotEmpty(t, r.Header.Get("X-Stripe-Client-User-Agent"))
+		assert.Equal(t, "websocket-random-id", r.Header.Get("Websocket-Id"))
+		c, err := upgrader.Upgrade(w, r, nil)
+		assert.Nil(t, err)
+
+		assert.Equal(t, "websocket_feature=request-log-payloads", r.URL.RawQuery)
+
+		defer c.Close()
+
+		evt := RequestLogEvent{
+			EventPayload: "{}",
+			RequestLogID: "resp_123",
+			Type: "request_log_event",
+		}
+
+		msg, err := json.Marshal(evt)
+		assert.Nil(t, err)
+
+		err = c.WriteMessage(ws.TextMessage, msg)
+		assert.Nil(t, err)
+	}))
+	defer ts.Close()
+
+	url := "ws" + strings.TrimPrefix(ts.URL, "http")
+
+	var rcvMsg *RequestLogEvent
+	client := NewClient(
+		url,
+		"websocket-random-id",
+		"request-log-payloads",
+		&Config{
+			EventHandler: EventHandlerFunc(func(msg IncomingMessage) {
+				rcvMsg = msg.RequestLogEvent
+				wg.Done()
+			}),
+		},
+	)
+	go client.Run()
+	defer client.Stop()
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(500 * time.Millisecond):
+		assert.FailNow(t, "Timed out waiting for response from test server")
+	}
+
+	assert.Equal(t, "resp_123", rcvMsg.RequestLogID)
+	assert.Equal(t, "request_log_event", rcvMsg.Type)
+	assert.Equal(t, "{}", rcvMsg.EventPayload)
+}
