45685927c9bd9574ed437fad08113ecd5b25b5e9
Tomer Elmalem <tomer@stripe.com>
2019-07-31 09:53:02 -0700

    Start cleaning up

--- pkg/recipes/recipes.go
+++ pkg/recipes/recipes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/manifoldco/promptui"
 	"github.com/stripe/stripe-cli/pkg/ansi"
@@ -37,53 +38,43 @@ func (r *Recipes) Download(app string) (string, error) {
 	return appPath, nil
 }
 
-func (r *Recipes) BuildPrompts(repoPath string) (string, string, error) {
+func (r *Recipes) BuildPrompts(repoPath string) ([]string, string, error) {
 	var language string
-	var integration string
+	var integration []string
 
 	topLevelFolders, err := r.GetFolders(repoPath)
 	if err != nil {
-		return "", "", err
+		return []string{}, "", err
 	}
 
 	if folderSearch(topLevelFolders, "server") {
 		languages, err := r.GetFolders(filepath.Join(repoPath, "server"))
 		if err != nil {
-			return "", "", err
+			return []string{}, "", err
 		}
 
 		language = languageSelectPrompt(languages)
 	} else {
 		integrations, err := r.GetFolders(repoPath)
 		if err != nil {
-			return "", "", err
+			return []string{}, "", err
 		}
 		integrations = append(integrations, "all")
 
 		integration = integrationSelectPrompt(integrations)
 
-		var languages []string
-		if integration == "all" {
-			// All integrations will have the same language support so we can just pull the langauges
-			// for the first integration type
-			languages, err = r.GetFolders(filepath.Join(repoPath, integrations[0], "server"))
-			if err != nil {
-				return "", "", err
-			}
-		} else {
-			languages, err = r.GetFolders(filepath.Join(repoPath, integration, "server"))
-			if err != nil {
-				return "", "", err
-			}
+		// All integrations will have the same language support so we can just pull the langauges
+		// for the first integration type
+		languages, err := r.GetFolders(filepath.Join(repoPath, integrations[0], "server"))
+		if err != nil {
+			return []string{}, "", err
 		}
 
 		language = languageSelectPrompt(languages)
 	}
 
-	if integration == "all" {
-		fmt.Println("Setting up", ansi.Bold(language), "for", ansi.Bold(integration), "integrations")
-	} else if integration != "" {
-		fmt.Println("Setting up", ansi.Bold(language), "for", ansi.Bold(integration))
+	if len(integration) > 0 {
+		fmt.Println("Setting up", ansi.Bold(language), "for", ansi.Bold(strings.Join(integration, ",")))
 	} else {
 		fmt.Println("Setting up", ansi.Bold(language))
 	}
@@ -111,6 +102,11 @@ func languageSelectPrompt(languages []string) string {
 	return selectOptions("What language would you like to use?", languages)
 }
 
-func integrationSelectPrompt(integrations []string) string {
-	return selectOptions("What type of integration would you like to use?", integrations)
+func integrationSelectPrompt(integrations []string) []string {
+	selected := selectOptions("What type of integration would you like to use?", integrations)
+	if selected == "all" {
+		return integrations
+	}
+
+	return []string{selected}
 }
