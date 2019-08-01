344859caecb1d7817bffe4486805e0852596dc47
Aaron Green <aarongreen@stripe.com>
2019-08-01 13:57:18 -0700

    output formatting

--- pkg/websocket/request_log_messages.go
+++ pkg/websocket/request_log_messages.go
@@ -3,6 +3,6 @@ package websocket
 // RequestLogEvent represents incoming request log event messages sent by Stripe.
 type RequestLogEvent struct {
 	EventPayload string `json:"event_payload"`
-	Type         string `json:"type"`
 	RequestLogID string `json:"request_log_id"`
+	Type         string `json:"type"`
 }
