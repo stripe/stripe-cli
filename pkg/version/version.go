0511922dad1dcb640184c1ef0fede3712c938b1c
Tomer Elmalem <tomer@stripe.com>
2019-08-02 10:46:21 -0700

    Add -v flag for version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,4 +1,12 @@
 package version
 
+import (
+	"fmt"
+
+	"github.com/stripe/stripe-cli/pkg/ansi"
+)
+
 // Version number of the CLI
 var Version = "master"
+
+var Template = fmt.Sprintf("stripe version %s %s\n", Version, ansi.Bold("(beta)"))
