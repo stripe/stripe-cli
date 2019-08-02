f36b01ac382a90bfea49db7856e116a2e0ee1159
Tomer Elmalem <tomer@stripe.com>
2019-08-02 11:56:25 -0700

    Add a comment explaining how the version flag is set

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,7 +6,10 @@ import (
 	"github.com/stripe/stripe-cli/pkg/ansi"
 )
 
-// Version number of the CLI
+// Version of the CLI -- currently in beta
+// This is set to the actual version by GoReleaser, identify by the
+// git tag assigned to the release Versions built from source will
+// always show master.
 var Version = "master"
 
 var Template = fmt.Sprintf("stripe version %s %s\n", Version, ansi.Bold("(beta)"))
