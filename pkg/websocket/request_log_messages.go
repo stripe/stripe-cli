b1c112ba3282bb241da86ec99c7d9274dd0bc748
Aaron Green <aarongreen@stripe.com>
2019-08-06 11:52:10 -0700

    add requestlogid comment clarification

--- pkg/websocket/request_log_messages.go
+++ pkg/websocket/request_log_messages.go
@@ -1,6 +1,10 @@
 package websocket
 
 // RequestLogEvent represents incoming request log event messages sent by Stripe.
+
+// RequestLogID is the `resp_` id for the actual response event which is used as the request log event throughout the system.
+// This is different from the `EventPayload.RequestID` which is the `req_` id for the user's actual request, which they
+// can use to find their request in the dashboard.
 type RequestLogEvent struct {
 	EventPayload string `json:"event_payload"`
 	RequestLogID string `json:"request_log_id"`
