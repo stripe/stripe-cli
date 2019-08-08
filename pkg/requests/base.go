5d463d2da87b1a19b6423fadc673c4335f367d76
Tomer Elmalem <tomer@stripe.com>
2019-08-08 13:17:21 -0700

    Remove extra println

--- pkg/requests/base.go
+++ pkg/requests/base.go
@@ -50,7 +50,6 @@ var confirmationCommands = map[string]bool{http.MethodDelete: true}
 
 // RunRequestsCmd is the interface exposed for the CLI to run network requests through
 func (rb *Base) RunRequestsCmd(cmd *cobra.Command, args []string) error {
-	fmt.Print(rb.Profile.ProfileName)
 	if len(args) > 1 {
 		return fmt.Errorf("this command only supports one argument. Run with the --help flag to see usage and examples")
 	}
