bf9a9ed340ecad23cbce58ff692254be29d171a6
Tomer Elmalem <tomer@stripe.com>
2019-08-26 12:08:25 -0700

    Print out list of shortcut urls and sort the list

--- pkg/cmd/open.go
+++ pkg/cmd/open.go
@@ -2,50 +2,51 @@ package cmd
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/stripe/stripe-cli/pkg/open"
-	"github.com/stripe/stripe-cli/pkg/validators"
 )
 
 var nameURLmap = map[string]string{
-	"dashboard":                          "https://dashboard.stripe.com/",
-	"dashboard/payments":                 "https://dashboard.stripe.com/payments",
-	"dashboard/disputes":                 "https://dashboard.stripe.com/disputes",
-	"dashboard/balance":                  "https://dashboard.stripe.com/balance/overview",
-	"dashboard/payouts":                  "https://dashboard.stripe.com/payouts",
-	"dashboard/topups":                   "https://dashboard.stripe.com/topups",
-	"dashboard/transactions":             "https://dashboard.stripe.com/balance",
-	"dashboard/customers":                "https://dashboard.stripe.com/customers",
-	"dashboard/atlas":                    "https://dashboard.stripe.com/atlas",
-	"dashboard/radar":                    "https://dashboard.stripe.com/radar",
-	"dashboard/radar/reviews":            "https://dashboard.stripe.com/radar/reviews",
-	"dashboard/radar/list":               "https://dashboard.stripe.com/radar/list",
-	"dashboard/radar/rules":              "https://dashboard.stripe.com/radar/rules",
-	"dashboard/billing":                  "https://dashboard.stripe.com/billing",
-	"dashboard/invoices":                 "https://dashboard.stripe.com/invoices",
-	"dashboard/subscriptions":            "https://dashboard.stripe.com/subscriptions",
-	"dashboard/subscriptions/products":   "https://dashboard.stripe.com/subscriptions/products",
-	"dashboard/tax-rates":                "https://dashboard.stripe.com/tax-rates",
-	"dashboard/coupons":                  "https://dashboard.stripe.com/coupons",
-	"dashboard/connect":                  "https://dashboard.stripe.com/connect/overview",
-	"dashboard/connect/accounts":         "https://dashboard.stripe.com/connect/accounts/overview",
-	"dashboard/connect/transfers":        "https://dashboard.stripe.com/connect/transfers",
-	"dashboard/connect/collected-fees":   "https://dashboard.stripe.com/connect/application_fees",
-	"dashboard/orders":                   "https://dashboard.stripe.com/orders",
-	"dashboard/orders/products":          "https://dashboard.stripe.com/orders/products",
-	"dashboard/terminal":                 "https://dashboard.stripe.com/terminal",
-	"dashboard/terminal/locations":       "https://dashboard.stripe.com/terminal/locations",
-	"dashboard/terminal/hardware_orders": "https://dashboard.stripe.com/terminal/hardware_orders",
-	"dashboard/developers":               "https://dashboard.stripe.com/developers",
-	"dashboard/apikeys":                  "https://dashboard.stripe.com/apikeys",
-	"dashboard/webhooks":                 "https://dashboard.stripe.com/webhooks",
-	"dashboard/events":                   "https://dashboard.stripe.com/events",
-	"dashboard/logs":                     "https://dashboard.stripe.com/logs",
-	"dashboard/settings":                 "https://dashboard.stripe.com/settings",
 	"api":                                "https://stripe.com/docs/api",
 	"apiref":                             "https://stripe.com/docs/api",
+	"dashboard":                          "https://dashboard.stripe.com/test",
+	"dashboard/apikeys":                  "https://dashboard.stripe.com/test/apikeys",
+	"dashboard/atlas":                    "https://dashboard.stripe.com/test/atlas",
+	"dashboard/balance":                  "https://dashboard.stripe.com/test/balance/overview",
+	"dashboard/billing":                  "https://dashboard.stripe.com/test/billing",
+	"dashboard/connect":                  "https://dashboard.stripe.com/test/connect/overview",
+	"dashboard/connect/accounts":         "https://dashboard.stripe.com/test/connect/accounts/overview",
+	"dashboard/connect/collected-fees":   "https://dashboard.stripe.com/test/connect/application_fees",
+	"dashboard/connect/transfers":        "https://dashboard.stripe.com/test/connect/transfers",
+	"dashboard/coupons":                  "https://dashboard.stripe.com/test/coupons",
+	"dashboard/customers":                "https://dashboard.stripe.com/test/customers",
+	"dashboard/developers":               "https://dashboard.stripe.com/test/developers",
+	"dashboard/disputes":                 "https://dashboard.stripe.com/test/disputes",
+	"dashboard/events":                   "https://dashboard.stripe.com/test/events",
+	"dashboard/invoices":                 "https://dashboard.stripe.com/test/invoices",
+	"dashboard/logs":                     "https://dashboard.stripe.com/test/logs",
+	"dashboard/orders":                   "https://dashboard.stripe.com/test/orders",
+	"dashboard/orders/products":          "https://dashboard.stripe.com/test/orders/products",
+	"dashboard/payments":                 "https://dashboard.stripe.com/test/payments",
+	"dashboard/payouts":                  "https://dashboard.stripe.com/test/payouts",
+	"dashboard/radar":                    "https://dashboard.stripe.com/test/radar",
+	"dashboard/radar/list":               "https://dashboard.stripe.com/test/radar/list",
+	"dashboard/radar/reviews":            "https://dashboard.stripe.com/test/radar/reviews",
+	"dashboard/radar/rules":              "https://dashboard.stripe.com/test/radar/rules",
+	"dashboard/settings":                 "https://dashboard.stripe.com/test/settings",
+	"dashboard/subscriptions":            "https://dashboard.stripe.com/test/subscriptions",
+	"dashboard/subscriptions/products":   "https://dashboard.stripe.com/test/subscriptions/products",
+	"dashboard/tax-rates":                "https://dashboard.stripe.com/test/tax-rates",
+	"dashboard/terminal":                 "https://dashboard.stripe.com/test/terminal",
+	"dashboard/terminal/hardware_orders": "https://dashboard.stripe.com/test/terminal/hardware_orders",
+	"dashboard/terminal/locations":       "https://dashboard.stripe.com/test/terminal/locations",
+	"dashboard/topups":                   "https://dashboard.stripe.com/test/topups",
+	"dashboard/transactions":             "https://dashboard.stripe.com/test/balance",
+	"dashboard/webhooks":                 "https://dashboard.stripe.com/test/webhooks",
 	"docs":                               "https://stripe.com/docs",
 }
 
@@ -58,6 +59,28 @@ func openNames() []string {
 	return keys
 }
 
+func getLongestShortcut(shortcuts []string) int {
+	longest := 0
+	for _, s := range shortcuts {
+		if len(s) > longest {
+			longest = len(s)
+		}
+	}
+
+	return longest
+}
+
+func padName(name string, length int) string {
+	difference := length - len(name)
+	var b strings.Builder
+	fmt.Fprint(&b, name)
+	for i := 0; i < difference; i++ {
+		fmt.Fprint(&b, " ")
+	}
+
+	return b.String()
+}
+
 type openCmd struct {
 	cmd *cobra.Command
 }
@@ -66,16 +89,35 @@ func newOpenCmd() *openCmd {
 	oc := &openCmd{}
 	oc.cmd = &cobra.Command{
 		Use:       "open",
-		Args:      validators.ExactArgs(1),
 		ValidArgs: openNames(),
 		Short:     "Quickly open Stripe pages",
 		RunE:      oc.runOpenCmd,
 	}
 
+	oc.cmd.Flags().Bool("list", false, "List all supported short cuts")
+
 	return oc
 }
 
 func (oc *openCmd) runOpenCmd(cmd *cobra.Command, args []string) error {
+	list, err := cmd.Flags().GetBool("list")
+	if err != nil {
+		return err
+	}
+
+	if list {
+		shortcuts := openNames()
+		sort.Strings(shortcuts)
+		longest := getLongestShortcut(shortcuts)
+
+		for _, shortcut := range shortcuts {
+			paddedName := padName(shortcut, longest)
+			fmt.Println(fmt.Sprintf("%s => %s", paddedName, nameURLmap[shortcut]))
+		}
+
+		return nil
+	}
+
 	if url, ok := nameURLmap[args[0]]; ok {
 		err := open.Browser(url)
 		if err != nil {
