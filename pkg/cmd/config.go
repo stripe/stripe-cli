f44ab0e1ac039c1883079ecaad26af375eecbbbe
Stephanie Chen <stephchen@stripe.com>
2019-09-17 15:47:21 -0700

    add explicit --set flag for setting config fields

--- pkg/cmd/config.go
+++ pkg/cmd/config.go
@@ -13,6 +13,7 @@ type configCmd struct {
 	list  bool
 	edit  bool
 	unset string
+	set bool
 }
 
 func newConfigCmd() *configCmd {
@@ -28,18 +29,21 @@ func newConfigCmd() *configCmd {
 	cc.cmd.Flags().BoolVar(&cc.list, "list", false, "list configs")
 	cc.cmd.Flags().BoolVarP(&cc.edit, "edit", "e", false, "open editor to the config file")
 	cc.cmd.Flags().StringVar(&cc.unset, "unset", "", "unset a specific config field")
+	cc.cmd.Flags().BoolVar(&cc.set, "set string string", false, "set a config field to some value")
+
+	cc.cmd.Flags().SetInterspersed(false) // allow args to happen after flags to enable 2 arguments to --set
 
 	return cc
 }
 
 func (cc *configCmd) runConfigCmd(cmd *cobra.Command, args []string) error {
 	switch ok := true; ok {
-	case len(args) == 2:
+	case cc.set && len(args) == 2:
 		return cc.config.Profile.WriteConfigField(args[0], args[1])
 	case cc.unset != "":
 		return cc.config.Profile.DeleteConfigField(cc.unset)
 	case cc.list:
-		cc.config.PrintConfig()
+		return cc.config.PrintConfig()
 	case cc.edit:
 		return cc.config.EditConfig()
 	}
