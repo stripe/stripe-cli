840b68fb2c9bcd369bea3b1d47b6bb27432d9a7e
stephchen-stripe <stephchen@stripe.com>
2019-09-18 09:45:17 -0700

    small fixes to `stripe config` flags (#171)
    * make config --unset flag take a string arg
    
    * add explicit --set flag for setting config fields
    
    * default to showing help page when no flags set
    
    * linting

--- pkg/cmd/config.go
+++ pkg/cmd/config.go
@@ -12,7 +12,8 @@ type configCmd struct {
 
 	list  bool
 	edit  bool
-	unset bool
+	unset string
+	set   bool
 }
 
 func newConfigCmd() *configCmd {
@@ -27,22 +28,26 @@ func newConfigCmd() *configCmd {
 
 	cc.cmd.Flags().BoolVar(&cc.list, "list", false, "list configs")
 	cc.cmd.Flags().BoolVarP(&cc.edit, "edit", "e", false, "open editor to the config file")
-	cc.cmd.Flags().BoolVar(&cc.unset, "unset", false, "unset a specific config field")
+	cc.cmd.Flags().StringVar(&cc.unset, "unset", "", "unset a specific config field")
+	cc.cmd.Flags().BoolVar(&cc.set, "set string string", false, "set a config field to some value")
+
+	cc.cmd.Flags().SetInterspersed(false) // allow args to happen after flags to enable 2 arguments to --set
 
 	return cc
 }
 
 func (cc *configCmd) runConfigCmd(cmd *cobra.Command, args []string) error {
 	switch ok := true; ok {
-	case len(args) == 2:
+	case cc.set && len(args) == 2:
 		return cc.config.Profile.WriteConfigField(args[0], args[1])
-	case len(args) == 1 && cc.unset:
-		return cc.config.Profile.DeleteConfigField(args[0])
+	case cc.unset != "":
+		return cc.config.Profile.DeleteConfigField(cc.unset)
 	case cc.list:
-		cc.config.PrintConfig()
+		return cc.config.PrintConfig()
 	case cc.edit:
 		return cc.config.EditConfig()
+	default:
+		// no flags set or unrecognized flags/args
+		return cc.cmd.Help()
 	}
-
-	return nil
 }
