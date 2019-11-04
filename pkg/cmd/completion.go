952e403d094396cc189d18be8abf2796a3aeb2aa
tomer-stripe <42354557+tomer-stripe@users.noreply.github.com>
2019-11-04 10:20:34 -0800

    Try to automatically detect shell (#274)
    * Try to automatically detect shell
    
    * Use shell env variable

--- pkg/cmd/completion.go
+++ pkg/cmd/completion.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -22,16 +24,42 @@ func newCompletionCmd() *completionCmd {
 		Short: "Generate bash and zsh completion scripts",
 		Args:  validators.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if cc.shell == "zsh" {
-				fmt.Println("Generated zsh completion file: stripe-completion.zsh")
-				return rootCmd.GenZshCompletionFile("stripe-completion.zsh")
-			}
-			fmt.Println("Generated bash completion file: stripe-completion.bash")
-			return rootCmd.GenBashCompletionFile("stripe-completion.bash")
+			return selectShell(cc.shell)
 		},
 	}
 
-	cc.cmd.Flags().StringVar(&cc.shell, "shell", "bash", "The shell to generate completion commands for. Supports \"bash\" or \"zsh\"")
+	cc.cmd.Flags().StringVar(&cc.shell, "shell", "", "The shell to generate completion commands for. Supports \"bash\" or \"zsh\"")
 
 	return cc
+}
+
+func selectShell(shell string) error {
+	selected := shell
+	if selected == "" {
+		selected = detectShell()
+	}
+
+	switch {
+	case selected == "zsh":
+		fmt.Println("Detected `zsh`, generating zsh completion file: stripe-completion.zsh")
+		return rootCmd.GenZshCompletionFile("stripe-completion.zsh")
+	case selected == "bash":
+		fmt.Println("Detected `bash`, generating bash completion file: stripe-completion.bash")
+		return rootCmd.GenBashCompletionFile("stripe-completion.bash")
+	default:
+		return fmt.Errorf("Could not automatically detect your shell. Please run the command with the `--shell` flag for either bash or zsh")
+	}
+}
+
+func detectShell() string {
+	shell := os.Getenv("SHELL")
+
+	switch {
+	case strings.Contains(shell, "zsh"):
+		return "zsh"
+	case strings.Contains(shell, "bash"):
+		return "bash"
+	default:
+		return ""
+	}
 }
