1687cec7bdf7aaa485765188a724f6687c6bccaf
tomer-stripe <42354557+tomer-stripe@users.noreply.github.com>
2019-11-13 12:56:24 -0800

    Add device name and generated flag (#312)

--- pkg/stripe/telemetry.go
+++ pkg/stripe/telemetry.go
@@ -15,12 +15,28 @@ import (
 // CLITelemetry is the structure that holds telemetry data sent to Stripe in
 // API requests.
 type CLITelemetry struct {
-	CommandPath string `json:"command_path"`
+	CommandPath       string `json:"command_path"`
+	DeviceName        string `json:"device_name"`
+	GeneratedResource bool   `json:"generated_resource"`
 }
 
 // SetCommandContext sets the telemetry values for the command being executed.
 func (t *CLITelemetry) SetCommandContext(cmd *cobra.Command) {
 	t.CommandPath = cmd.CommandPath()
+	t.GeneratedResource = false
+
+	for _, value := range cmd.Annotations {
+		// Generated commands have an annotation called "operation", we can
+		// search for that to let us know it's generated
+		if value == "operation" {
+			t.GeneratedResource = true
+		}
+	}
+}
+
+// SetDeviceName puts the device name into telemetry
+func (t *CLITelemetry) SetDeviceName(deviceName string) {
+	t.DeviceName = deviceName
 }
 
 //
