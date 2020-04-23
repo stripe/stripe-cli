cd408df9de81e1a2b735c36b02c7874b15103e5d
Ian Jabour <ianjabour@stripe.com>
2020-04-23 14:17:55 -0700

    Add logic to print message if user is already logged out when running logout command

--- pkg/logout/logout.go
+++ pkg/logout/logout.go
@@ -9,14 +9,21 @@ import (
 
 // Logout function is used to clear the credentials set for the current Profile
 func Logout(config *config.Config, input io.Reader) error {
-	fmt.Println("Logging out...")
+	liveKey, _ := config.Profile.GetAPIKey(true)
+	testKey, _ := config.Profile.GetAPIKey(false)
 
-	err := config.Profile.ClearKeys()
-	if err != nil {
-		return err
+	if liveKey == "" && testKey == "" {
+		fmt.Println("You are already logged out.")
+	} else {
+		fmt.Println("Logging out...")
+
+		err := config.Profile.ClearKeys()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("You are now logged out.")
 	}
-
-	fmt.Println("You are now logged out!")
 
 	return nil
 }
