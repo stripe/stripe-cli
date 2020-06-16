042733156b49cefd6a901ffb4d2495f74d0902e3
Brandon Wang <bwang@stripe.com>
2020-06-16 13:17:01 -0400

    Started working on abstracting some logic into Cassette.
    Next, switching gears a bit and trying to focus on getting a more general, but still concrete implementation working.

--- base.go
+++ base.go
@@ -1,20 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Example of concrete implentation: VCR as HTTP proxy
-var vcr = Vcr{}
+var vcr = Vcr{recordMode: true}
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// response := vcr.AcceptRequest(r)
+	fmt.Println("Handling request")
+
+	// --- pass to VCR, get response back
+	resp := vcr.AcceptRequest(r)
+	// -- end VCR interface
+
+	defer resp.Body.Close() // we need to close the body
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 
 	// take response and write the httpResponse
-	fmt.Fprintf(w, "Hello")
+	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", resp.Header.Get("Content-Length"))
+	io.Copy(w, resp.Body)
+
+	fmt.Fprintf(w, "hello")
 }
+
+// func main() {
+
+// 	resp, err := http.Get("http://gobyexample.com")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	fmt.Println("Response status:", resp.Status)
+
+// 	fmt.Printf("%s", resp.Body)
+
+// 	// scanner := bufio.NewScanner(resp.Body)
+// 	// for i := 0; scanner.Scan() && i < 5; i++ {
+// 	// 	fmt.Println(scanner.Text())
+// 	// }
+
+// 	// if err := scanner.Err(); err != nil {
+// 	// 	panic(err)
+// 	// }
+// }
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -25,7 +69,7 @@ func main() {
 
 type Vcr struct {
 	recordMode bool // true = record, false = replay
-
+	fileHandle os.File
 }
 
 type Request interface {
@@ -37,11 +81,30 @@ type Request interface {
 type Response interface {
 }
 
+func (vcr *Vcr) StartRecording(filepath string) error {
+	// open a cassette file for writing
+	fileHandle, err := os.Create(filepath)
+
+	if err != nil {
+		return err
+	}
+
+	vcr.fileHandle = *fileHandle
+
+	return nil
+}
+
+func (vcr *Vcr) StopRecording() {
+	vcr.fileHandle.Close()
+}
+
 // Call to expose the VCR to the next event *in the sequence*.
-func (vcr *Vcr) AcceptRequest(request Request) Response {
+// TODO: separate the functions for accept request in record vs replay mode
+// VCRRecorder vs VCRReplayer
+func (vcr *Vcr) AcceptRequest(request *http.Request) *http.Response {
 
 	// figure out the response
-	var response *Response
+	var response *http.Response
 	var err error
 
 	if vcr.recordMode {
@@ -56,7 +119,7 @@ func (vcr *Vcr) AcceptRequest(request Request) Response {
 
 	// update the recording, if in record mode
 	if vcr.recordMode {
-		vcr.updateRecording(request)
+		vcr.updateRecording(request, response)
 	}
 
 	// return the response
@@ -65,20 +128,60 @@ func (vcr *Vcr) AcceptRequest(request Request) Response {
 }
 
 // --- Private helpers
-func (vcr *Vcr) sendResponseToClient(response Response) {
+func (vcr *Vcr) sendResponseToClient(response http.Response) {
 
 }
 
-func (vcr *Vcr) updateRecording(request Request) {
+// Save a request/response pair to the recording
+func (vcr *Vcr) updateRecording(request *http.Request, response *http.Response) {
+	// serilaize the request to the cassette file
+
+	// TODO: for now, just recording the received URL path
+	_, err := vcr.fileHandle.WriteString("-> " + request.URL.Path)
+	_, err = vcr.fileHandle.WriteString("<- " + request.URL.Path)
+
+	if err != nil {
+		panic(err)
+	}
 
 }
 
-func (vcr *Vcr) getResponseFromRemote(request Request) (resp *Response, err error) {
-	return nil, nil
+func (vcr *Vcr) getResponseFromRemote(request *http.Request) (resp *http.Response, err error) {
+	// TODO: placeholder proxy a request to some random website. Later - this should pass on the request
+	res, err := http.Get("http://gobyexample.com")
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%s", res.Body)
+
+	// If returning the response to code that expects to read it, we cannot call res.Body.Close() here.
+	// defer res.Body.Close()
+
+	return res, nil
 }
 
 // returns error if something doesn't match the cassette
-func (vcr *Vcr) getNextRecordedCassetteResponse(request Request) (resp *Response, err error) {
+func (vcr *Vcr) getNextRecordedCassetteResponse(request *http.Request) (resp *http.Response, err error) {
 	// the passed in request arg may not be necessary
 	return nil, nil
 }
+
+// Test: VCRRecoder VCRReplayer
+/*
+
+try to write a test case
+
+// recorder.Write('a' -> 'A')
+// recorder.Write('b')
+// recorder.Write('c')
+// buf := recorder.Close()
+//
+// replayer.Open(buf)
+// resp := replayer.Write('a')
+//
+
+
+
+*/
---
+++ cassette.go
@@ -0,0 +1,34 @@
+type Cassette struct {
+	recordMode bool
+
+}
+
+
+func NewEmptyCassette(filepath) *Cassette {
+
+} 
+
+func ReadExistingCassette(filepath) *Cassette {
+
+}
+
+// TODO: this part of the interface is where the specifics of how we're implementing recording/replaying start to matter
+
+// --- Replay mode interface
+
+// If the request matches the history, return the associated response. Otherwise return an descriptive error.
+func AdvanceReplay(request *http.Request) (resp *http.Response, err error) {
+	if recordMode {
+		return nil, errors.New("Can't AdvanceReplay while in Record mode!")
+	}
+
+	// potentially meaty logic thats related to decisions about how we implement matching up replay requests to recorded history
+}
+
+// --- Record mode interface
+
+func RecordEvent(request *http.Request, response *http.Response) error{
+	if !recordMode {
+		return errors.New("Can't record while in replay mode!")
+	}
+}
