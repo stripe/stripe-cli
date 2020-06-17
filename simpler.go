9b73439ec24ca2d0b86f760c4db6cf85acc7db9a
Brandon Wang <bwang@stripe.com>
2020-06-17 09:59:02 -0400

    Implement basic sequential functionality.

--- simpler.go
+++ simpler.go
@@ -33,8 +33,8 @@ func NewRecorder(filepath string) (recorder *VcrRecorder, err error) {
 }
 
 // takes a generic struct
-func (recorder *VcrRecorder) Write(event interface{}) error {
-	bytes, err := json.Marshal(event)
+func (recorder *VcrRecorder) Write(req interface{}, resp interface{}) error {
+	bytes, err := json.Marshal(Pair{req, resp})
 
 	if err != nil {
 		return err
@@ -52,7 +52,7 @@ func (recorder *VcrRecorder) Close() error {
 
 type VcrReplayer struct {
 	fileHandle   *os.File
-	events       []interface{}
+	events       []Pair
 	historyIndex int
 }
 
@@ -76,7 +76,7 @@ func NewReplayer(filepath string) (replayer VcrReplayer, err error) {
 		lineCount++
 	}
 
-	replayer.events = make([]interface{}, lineCount)
+	replayer.events = make([]Pair, lineCount)
 	idx := 0
 
 	file.Seek(0, 0)
@@ -91,13 +91,18 @@ func NewReplayer(filepath string) (replayer VcrReplayer, err error) {
 
 }
 
-func (replayer *VcrReplayer) Write(event interface{}) interface{} {
+func (replayer *VcrReplayer) Write(req interface{}) interface{} {
 	if replayer.historyIndex == len(replayer.events) {
 		return nil
 	}
-	response := replayer.events[replayer.historyIndex]
+	savedRequest := replayer.events[replayer.historyIndex].First
+	savedResponse := replayer.events[replayer.historyIndex].Second
+
+	// TODO: can check req vs saved req here
+	fmt.Printf("Received %s vs recored %s\n", req, savedRequest)
+
 	replayer.historyIndex++
-	return response
+	return savedResponse
 }
 
 func (replayer *VcrReplayer) Close() {
@@ -106,6 +111,17 @@ func (replayer *VcrReplayer) Close() {
 
 type Event struct {
 	Name string
+	Id   int
+}
+
+type Pair struct {
+	First  interface{}
+	Second interface{}
+}
+
+type BetterEvent struct {
+	Name string
+	Type string
 	Id   int
 }
 
@@ -120,12 +136,14 @@ func testBaseRecorder() {
 	fmt.Println("Recording...")
 
 	s1 := Event{Name: "Event 1", Id: 23}
-	fmt.Println(s1)
-	recorder.Write(s1)
+	r1 := Event{Name: "Response 1", Id: 23}
+	fmt.Printf("%s | %s\n", s1, r1)
+	recorder.Write(s1, r1)
 
 	s2 := Event{Name: "Event 2", Id: 46}
-	fmt.Println(s2)
-	recorder.Write(s2)
+	r2 := Event{Name: "Event 2", Id: 46}
+	fmt.Printf("%s | %s\n", s2, r2)
+	recorder.Write(s2, r2)
 
 	err = recorder.Close()
 
@@ -136,14 +154,70 @@ func testBaseRecorder() {
 	replayer, err := NewReplayer(filepath)
 
 	fmt.Println("Replaying...")
-	fmt.Println(replayer.Write(Event{"placeholder", 2}))
-	fmt.Println(replayer.Write(Event{"placeholder", 2}))
+	fmt.Println(replayer.Write(s1))
+	fmt.Println(replayer.Write(s2))
+}
 
+func testBaseRecorder2() {
+	filepath := "test.txt"
+	recorder, err := NewRecorder(filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Recording...")
+
+	s1 := Event{Name: "Event 1", Id: 23}
+	r1 := Event{Name: "Response 1", Id: 23}
+	fmt.Printf("%s | %s\n", s1, r1)
+	recorder.Write(s1, r1)
+
+	s2 := Event{Name: "Event 2", Id: 46}
+	r2 := Event{Name: "Event 2", Id: 46}
+	fmt.Printf("%s | %s\n", s2, r2)
+	recorder.Write(s2, r2)
+
+	err = recorder.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	replayer, err := NewReplayer(filepath)
+
+	fmt.Println("Replaying...")
+	fmt.Println(replayer.Write(s2))
+	fmt.Println(replayer.Write(s1))
 }
 
 func main() {
 	testBaseRecorder()
 }
+
+/*
+	Notes 5pm
+	- could we do configurable matching?
+		func compare(req1, req2) bool {...}
+	- what should happen if you do something that is different than is recorded?
+	- can we not impose the sequential constraint? -- aka otherways of advancing/matching
+		- where we can re-order and still work (eg: match names) <<<<<< write a test for this
+	- ^^^
+	- have the structs always have a comprable / idetnity function
+
+	if we can support all of the below behaviors by taking in a matchign/comrpable function and applying it in some way... then its a good sign for our abstraction
+	- always return the responses in sequence
+	- return responses in sequence so long as they "match", if they don't match, throw an error
+	- take requests in, and return the first response that "matches", if it exists, throw an error
+	- take requests in, and return the last response that "matches", if it exists, throw an error
+
+	also think about templating with an eye towards dealing with the fake id issues we heard about
+
+	make use of automated test cases that run automatically
+
+
+
+*/
 
 // Test: VCRRecoder VCRReplayer
 /*
