361d78da5394a246eeb41955abfaaae106b1a430
richardm-stripe <52928443+richardm-stripe@users.noreply.github.com>
2020-06-22 17:05:14 -0400

    More helpful resource usage errors (#469)

--- pkg/cmd/root_test.go
+++ pkg/cmd/root_test.go
@@ -49,3 +49,22 @@ func TestHelpFlag(t *testing.T) {
 	require.Contains(t, output, "Stripe commands:")
 	require.NoError(t, err)
 }
+
+func TestExampleCommands(t *testing.T) {
+	{
+		_, err := executeCommand(rootCmd, "foo")
+		require.Equal(t, err.Error(), "unknown command \"foo\" for \"stripe\"")
+	}
+	{
+		_, err := executeCommand(rootCmd, "listen", "foo")
+		require.Equal(t, err.Error(), "`stripe listen` does not take any positional arguments. See `stripe listen --help` for supported flags and usage")
+	}
+	{
+		_, err := executeCommand(rootCmd, "post")
+		require.Equal(t, err.Error(), "`stripe post` requires exactly 1 positional argument. See `stripe post --help` for supported flags and usage")
+	}
+	{
+		_, err := executeCommand(rootCmd, "samples", "create", "foo", "foo", "foo")
+		require.Equal(t, err.Error(), "`stripe samples create` accepts at maximum 2 positional arguments. See `stripe samples create --help` for supported flags and usage")
+	}
+}
