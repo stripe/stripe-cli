stripe/stripe-cli

commit 63e526b5aebc8a7a7fc2122a50090d172df49aa9

Refactor so that mode and filepath are also part of the playback.Server constructor.
This lets us fully initialize the server from the CLI without needing to make any HTTP calls.

pkg/playback/http_playback.go
@@ -78,7 +78,7 @@ type Server struct {
 	httpReplayer replayServer
 
 	remoteURL         string
-	cassetteDirectory string // root directory for all cassette filepaths
+	cassetteDirectory string // absolute path to the root directory for all cassette filepaths
 
 	// state machine state
 	mode                  string // the user specified state (auto, record, replay)
@@ -86,65 +86,33 @@ type Server struct {
 	cassetteLoaded        bool
 }
 
-// NewServer instantiates a Server struct, representing the configuration of a playback proxy server
-func NewServer(remoteURL string, webhookURL string, cassetteDirectory string) (server *Server, err error) {
+// NewServer instantiates a Server struct, representing the configuration and current state of a playback proxy server
+// The cassetteDirectory param must be an absolute path
+// initialCasssetteFilepath can be a relative path (inretpreted relative to cassetteDirectory) or an absolute path
+func NewServer(remoteURL string, webhookURL string, absCassetteDirectory string, mode string, initialCassetteFilepath string) (server *Server, err error) {
 	server = &Server{}
-	server.mode = Auto
-	server.cassetteLoaded = false
-	server.remoteURL = remoteURL
-	server.cassetteDirectory = cassetteDirectory
 
+	err = server.switchMode(mode)
+	if err != nil {
+		return server, err
+	}
+
+	err = server.loadCassette(initialCassetteFilepath)
+	if err != nil {
+		return server, err
+	}
+	server.cassetteLoaded = true
+
+	err = server.setCassetteDir(absCassetteDirectory)
+	if err != nil {
+		return server, err
+	}
+
+	server.remoteURL = remoteURL
 	server.httpRecorder = newRecordServer(remoteURL, webhookURL)
 	server.httpReplayer = newReplayServer(webhookURL)
 
 	return server, nil
-}
-
-func (rr *Server) handler(w http.ResponseWriter, r *http.Request) {
-	if !rr.cassetteLoaded {
-		w.WriteHeader(400)
-		fmt.Fprint(w, "No cassette is loaded.\n")
-		return
-	}
-
-	switch rr.mode {
-	case Record:
-		rr.httpRecorder.handler(w, r)
-	case Replay:
-		rr.httpReplayer.handler(w, r)
-	case Auto:
-		if rr.isRecordingInAutoMode {
-			rr.httpRecorder.handler(w, r)
-		} else {
-			rr.httpReplayer.handler(w, r)
-		}
-	default:
-		// Should never get here
-		handleErrorInHandler(w, fmt.Errorf("got an unexpected playback mode \"%s\". It must be either \"record\", \"replay\", or \"auto\"", rr.mode), 500)
-		return
-	}
-}
-
-// Webhooks are handled slightly differently from outgoing API requests
-func (rr *Server) webhookHandler(w http.ResponseWriter, r *http.Request) {
-	if !rr.cassetteLoaded {
-		w.WriteHeader(400)
-		fmt.Fprint(w, "No cassette is loaded.\n")
-		return
-	}
-
-	switch rr.mode {
-	case Record:
-		rr.httpRecorder.webhookHandler(w, r)
-	case Replay:
-		fmt.Println("Error: webhook endpoint should never be called in replay mode")
-	case Auto:
-		if rr.isRecordingInAutoMode {
-			rr.httpRecorder.webhookHandler(w, r)
-		} else {
-			fmt.Println("Error: webhook endpoint should never be called in replay mode")
-		}
-	}
 }
 
 // InitializeServer sets up and returns a http.Server that acts as a playback proxy
@@ -286,6 +254,53 @@ func (rr *Server) InitializeServer(address string) *http.Server {
 	customMux.HandleFunc("/", rr.handler)
 
 	return server
+}
+
+func (rr *Server) handler(w http.ResponseWriter, r *http.Request) {
+	if !rr.cassetteLoaded {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "No cassette is loaded.\n")
+		return
+	}
+
+	switch rr.mode {
+	case Record:
+		rr.httpRecorder.handler(w, r)
+	case Replay:
+		rr.httpReplayer.handler(w, r)
+	case Auto:
+		if rr.isRecordingInAutoMode {
+			rr.httpRecorder.handler(w, r)
+		} else {
+			rr.httpReplayer.handler(w, r)
+		}
+	default:
+		// Should never get here
+		handleErrorInHandler(w, fmt.Errorf("got an unexpected playback mode \"%s\". It must be either \"record\", \"replay\", or \"auto\"", rr.mode), 500)
+		return
+	}
+}
+
+// Webhooks are handled slightly differently from outgoing API requests
+func (rr *Server) webhookHandler(w http.ResponseWriter, r *http.Request) {
+	if !rr.cassetteLoaded {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "No cassette is loaded.\n")
+		return
+	}
+
+	switch rr.mode {
+	case Record:
+		rr.httpRecorder.webhookHandler(w, r)
+	case Replay:
+		fmt.Println("Error: webhook endpoint should never be called in replay mode")
+	case Auto:
+		if rr.isRecordingInAutoMode {
+			rr.httpRecorder.webhookHandler(w, r)
+		} else {
+			fmt.Println("Error: webhook endpoint should never be called in replay mode")
+		}
+	}
 }
 
 func (rr *Server) switchMode(modeString string) error {

pkg/cmd/playback.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
 	"os"
 	"path/filepath"
 
@@ -126,9 +124,9 @@ func (pc *playbackCmd) runPlaybackCmd(cmd *cobra.Command, args []string) error {
 
 	webhookAddress := "http://" + pc.webhookAddress
 
-	httpWrapper, err := playback.NewServer(remoteURL, webhookAddress, absoluteCassetteDir)
+	httpWrapper, err := playback.NewServer(remoteURL, webhookAddress, absoluteCassetteDir, pc.mode, pc.filepath)
 	if err != nil {
-		return nil
+		return err
 	}
 
 	server := httpWrapper.InitializeServer(addressString)
@@ -137,31 +135,6 @@ func (pc *playbackCmd) runPlaybackCmd(cmd *cobra.Command, args []string) error {
 		fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
 	}()
-
-	fullAddressString := "http://" + addressString
-
-	client := &http.Client{}
-
-	// --- Use the flag values to configure the server (over HTTP)
-	resp, err := client.Get(fullAddressString + "/playback/mode/" + pc.mode)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return errors.New("Non 200 status code received during startup: " + resp.Status)
-	}
-
-	resp, err = client.Get(fullAddressString + "/playback/cassette/load?filepath=" + pc.filepath)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return errors.New("Non 200 status code received during startup: " + resp.Status)
-	}
 
 	// --- Print out post-startup summary on CLI
 	fmt.Println()

pkg/playback/http_playback_test.go
@@ -377,7 +377,7 @@ func TestPlaybackSingleRunCreateCustomerAndStandaloneCharge(t *testing.T) {
 	assert.NoError(t, err)
 
 	webhookURL := defaultLocalWebhookAddress // not used in this test
-	httpWrapper, err := NewServer(remoteURL, webhookURL, cassetteDirectory)
+	httpWrapper, err := NewServer(remoteURL, webhookURL, cassetteDirectory, Auto, cassetteFilepath)
 	assert.NoError(t, err)
 
 	server := httpWrapper.InitializeServer(addressString)

commit eec6ba5b6fc5c46ce2a3614c367125557d573c8e

Clean up a bug introduced in my refactor in 63e526b5aebc8a7a7fc2122a50090d172df49aa9.
Setup work being done in the playback.Server constructor was being overwritten by later field assignments in that same constructor. Fixed by initializing fields at the top, before all the other setup work.

pkg/playback/http_playback.go
@@ -77,6 +77,12 @@ type Server struct {
 func NewServer(remoteURL string, webhookURL string, absCassetteDirectory string, mode string, initialCassetteFilepath string) (server *Server, err error) {
 	server = &Server{}
 
+	// initialize server.httpRecorder and server.httpReplayer first, since calls to methods like
+	// server.loadCassette reference them.
+	server.httpRecorder = newRecordServer(remoteURL, webhookURL)
+	server.httpReplayer = newReplayServer(webhookURL)
+	server.remoteURL = remoteURL
+
 	err = server.switchMode(mode)
 	if err != nil {
 		return server, err
@@ -91,10 +97,6 @@ func NewServer(remoteURL string, webhookURL string, absCassetteDirectory string,
 	if err != nil {
 		return server, err
 	}
-
-	server.remoteURL = remoteURL
-	server.httpRecorder = newRecordServer(remoteURL, webhookURL)
-	server.httpReplayer = newReplayServer(webhookURL)
 
 	return server, nil
 }