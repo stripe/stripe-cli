e740188caeaa11df8c79f29f5c420b68a41b7aa6
Hector <97482672+haor1@users.noreply.github.com>
2022-04-23 22:35:25 +0100

    Update stripe-completion.bash

--- stripe-completion.bash
+++ stripe-completion.bash
@@ -31935,6 +31935,7 @@ _stripe_trigger()
     must_have_one_noun+=("subscription_schedule.created")
     must_have_one_noun+=("subscription_schedule.released")
     must_have_one_noun+=("subscription_schedule.updated")
+    must_have_one_noun+=("transfer.paid")
     noun_aliases=()
 }
 
