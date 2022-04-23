6a8bcbba9947419cb6d3449618cef53207c5a71d
Hector <97482672+haor1@users.noreply.github.com>
2022-04-23 22:38:24 +0100

    Update triggers.go

--- pkg/fixtures/triggers.go
+++ pkg/fixtures/triggers.go
@@ -78,7 +78,7 @@ var Events = map[string]string{
 	"quote.canceled":                           "triggers/quote.canceled.json",
 	"quote.finalized":                          "triggers/quote.finalized.json",
 	"quote.accepted":                           "triggers/quote.accepted.json",
-	"transfer.paid":                            "triggers/transfer.paid.json",
+	"transfer.created":                         "triggers/transfer.created.json",
 }
 
 // BuildFromFixtureFile creates a new fixture struct for a file
