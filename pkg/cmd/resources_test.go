a02b5b5de0bb8f09ab11bfb5eeee7041bdc8e263
Evelyn Tsai <etsai@stripe.com>
2022-06-21 16:42:28 -0700

    Verify resource command does not conflict with plugin command

--- pkg/cmd/resources_test.go
+++ pkg/cmd/resources_test.go
@@ -12,3 +12,20 @@ func TestResources(t *testing.T) {
 	require.Contains(t, output, "Available commands:")
 	require.NoError(t, err)
 }
+
+func TestConflictWithPluginCommand(t *testing.T) {
+	pluginCommands, err := getAllPluginCommands()
+	require.NoError(t, err)
+
+	for _, cmd := range rootCmd.Commands() {
+		for _, pluginCommand := range pluginCommands {
+			// TO-DO: this is a patch.
+			// this check and this patch PR https://github.com/stripe/stripe-cli/pull/887
+			// should be removed once openapi spec is updated to not use `apps`
+			if cmd.Use == "apps" {
+				continue
+			}
+			require.False(t, cmd.Use == pluginCommand)
+		}
+	}
+}
