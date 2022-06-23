ad4cf2aabf229c837f29ee95fb2f6187eab198e7
etsai-stripe <88805634+etsai-stripe@users.noreply.github.com>
2022-06-23 09:49:08 -0700

    Verify resource command does not conflict with plugin command (#900)
    * Verify resource command does not conflict with plugin command
    
    * make unexportable
    
    * rename symbol
    
    * fix lint
    
    * refactor
    
    * fix lint

--- pkg/cmd/resources_test.go
+++ pkg/cmd/resources_test.go
@@ -1,9 +1,18 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"net/http"
 	"testing"
 
+	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
+
+	"github.com/stripe/stripe-cli/pkg/plugins"
+)
+
+const (
+	pluginManifestURL = "https://stripe.jfrog.io/artifactory/stripe-cli-plugins-local/plugins.toml"
 )
 
 func TestResources(t *testing.T) {
@@ -11,4 +20,36 @@ func TestResources(t *testing.T) {
 
 	require.Contains(t, output, "Available commands:")
 	require.NoError(t, err)
+}
+
+func TestConflictWithPluginCommand(t *testing.T) {
+	// directly downloading the manifest can only be done within this unit test
+	// plugins.GetPluginList should be used under normal circumstances
+	resp, err := http.Get(pluginManifestURL)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	var manifest plugins.PluginList
+	err = toml.Unmarshal(respBytes, &manifest)
+	require.NoError(t, err)
+
+	var pluginCommands []string
+	for _, plugin := range manifest.Plugins {
+		pluginCommands = append(pluginCommands, plugin.Shortname)
+	}
+
+	for _, cmd := range rootCmd.Commands() {
+		for _, pluginCommand := range pluginCommands {
+			// TO-DO: this is a patch.
+			// this check and this patch PR https://github.com/stripe/stripe-cli/pull/887
+			// should be removed once openapi spec is updated to not use `apps`
+			if cmd.Use == "apps" {
+				continue
+			}
+			require.False(t, cmd.Use == pluginCommand)
+		}
+	}
 }
