cf232bf4757985835047c177007719ec0b12b7c1
Tomer Elmalem <telmalem@gmail.com>
2022-07-14 08:27:56 -0700

    Add comment for exported server function

--- pkg/rpcservice/rpc_service.go
+++ pkg/rpcservice/rpc_service.go
@@ -128,6 +128,7 @@ func (srv *RPCService) printConfig(configOutput ConfigOutput) {
 	}
 }
 
+// Server returns the gRPC server
 func (srv *RPCService) Server() *grpc.Server {
 	return srv.grpcServer
 }
