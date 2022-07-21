1f85fef326ee695f80fe746345f48f2c9edbae76
etsai-stripe <88805634+etsai-stripe@users.noreply.github.com>
2022-07-21 13:31:26 -0700

    DX-7509: redact livemode values in existing configs (#918)
    * DX-7509: redact livemode values in existing configs
    
    * fix import spacings

--- pkg/config/profile_livemode.go
+++ pkg/config/profile_livemode.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 
 	"github.com/99designs/keyring"
+
+	"github.com/stripe/stripe-cli/pkg/validators"
 )
 
 // DateStringFormat ...
@@ -27,10 +29,37 @@ func (p *Profile) storeLivemodeValue(field, value, description string) {
 }
 
 // RetrieveLivemodeValue ...
-func (p *Profile) RetrieveLivemodeValue(key string) string {
+func (p *Profile) RetrieveLivemodeValue(key string) (string, error) {
 	fieldID := p.GetConfigField(key)
-	value, _ := KeyRing.Get(fieldID)
-	return string(value.Data)
+	existingKeys, err := KeyRing.Keys()
+	if err != nil {
+		return "", err
+	}
+
+	for _, item := range existingKeys {
+		if item == fieldID {
+			value, _ := KeyRing.Get(fieldID)
+			return string(value.Data), nil
+		}
+	}
+
+	return "", validators.ErrAPIKeyNotConfigured
+}
+
+// DeleteLivemodeValue ...
+func (p *Profile) DeleteLivemodeValue(key string) error {
+	fieldID := p.GetConfigField(key)
+	existingKeys, err := KeyRing.Keys()
+	if err != nil {
+		return err
+	}
+	for _, item := range existingKeys {
+		if item == fieldID {
+			KeyRing.Remove(fieldID)
+			return nil
+		}
+	}
+	return nil
 }
 
 // RedactAPIKey returns a redacted version of API keys. The first 8 and last 4
@@ -45,4 +74,22 @@ func RedactAPIKey(apiKey string) string {
 	b.WriteString(apiKey[len(apiKey)-4:])              // #nosec G104 (gosec bug: https://github.com/securego/gosec/issues/267)
 
 	return b.String()
+}
+
+// IsRedactedAPIKey ...
+func IsRedactedAPIKey(apiKey string) bool {
+	keyParts := strings.Split(apiKey, "_")
+	if len(keyParts) < 3 {
+		return false
+	}
+
+	if keyParts[0] != "sk" && keyParts[0] != "rk" {
+		return false
+	}
+
+	if RedactAPIKey(apiKey) != apiKey {
+		return false
+	}
+
+	return true
 }
