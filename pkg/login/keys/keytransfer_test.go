596de3c459c065465b092e1fa305a83dd87b4025
Vincent Cheung <vcheung@stripe.com>
2023-02-23 14:15:28 -0800

    fix test

--- pkg/login/keys/keytransfer_test.go
+++ pkg/login/keys/keytransfer_test.go
@@ -17,11 +17,11 @@ import (
 )
 
 type mockConfigurer struct {
-	t *testing.T
+	called bool
 }
 
 func (c *mockConfigurer) SaveLoginDetails(response *PollAPIKeyResponse) error {
-	require.Equal(c.t, true, true, "expected SaveLoginDetails to be called, but wasn't")
+	c.called = true
 	return nil
 }
 
@@ -49,13 +49,14 @@ func TestAsyncPollKey_Succeeds(t *testing.T) {
 	defer ts.Close()
 
 	ch := make(chan AsyncPollResult)
-	configurer := mockConfigurer{t: t}
+	configurer := mockConfigurer{}
 	rt := NewRAKTransfer(&configurer)
 
 	go rt.AsyncPollKey(context.Background(), ts.URL, 1*time.Millisecond, 3, ch)
 
 	result := <-ch
 
+	require.True(t, configurer.called, "expected SaveLoginDetails to be called, but wasn't")
 	require.NoError(t, result.Err)
 	assert.Equal(t, "sk_test_123", result.TestModeAPIKey)
 	assert.Equal(t, &acct.Account{
@@ -80,13 +81,13 @@ func TestAsyncPollKey_ResponseError(t *testing.T) {
 	defer ts.Close()
 
 	ch := make(chan AsyncPollResult)
-	configurer := mockConfigurer{t: t}
+	configurer := mockConfigurer{}
 	rt := NewRAKTransfer(&configurer)
 
 	go rt.AsyncPollKey(context.Background(), ts.URL, 1*time.Millisecond, 3, ch)
 
 	result := <-ch
-
+	require.False(t, configurer.called, "SaveLoginDetails was unexpectedly called")
 	assert.EqualError(t, result.Err, "unexpected http status code: 500 ")
 }
 
@@ -108,22 +109,23 @@ func TestAsyncPollKey_ExceedMaxAttempts(t *testing.T) {
 	defer ts.Close()
 
 	ch := make(chan AsyncPollResult)
-	configurer := mockConfigurer{t: t}
+	configurer := mockConfigurer{}
 	rt := NewRAKTransfer(&configurer)
 
 	go rt.AsyncPollKey(context.Background(), ts.URL, 1*time.Millisecond, 3, ch)
 
 	result := <-ch
+	require.False(t, configurer.called, "SaveLoginDetails was unexpectedly called")
 	assert.EqualError(t, result.Err, "exceeded max attempts")
 	assert.Equal(t, uint64(3), atomic.LoadUint64(&attempts))
 }
 
 type errorConfigurer struct {
-	t *testing.T
+	called bool
 }
 
 func (c *errorConfigurer) SaveLoginDetails(response *PollAPIKeyResponse) error {
-	require.Equal(c.t, true, true, "expected SaveLoginDetails to be called, but wasn't")
+	c.called = true
 	return errors.New("failed to save login details")
 }
 
@@ -151,12 +153,12 @@ func TestAsyncPollKey_ConfigurerError(t *testing.T) {
 	defer ts.Close()
 
 	ch := make(chan AsyncPollResult)
-	configurer := errorConfigurer{t: t}
+	configurer := errorConfigurer{}
 	rt := NewRAKTransfer(&configurer)
 
 	go rt.AsyncPollKey(context.Background(), ts.URL, 1*time.Millisecond, 3, ch)
 
 	result := <-ch
-
+	require.True(t, configurer.called, "expected SaveLoginDetails to be called, but wasn't")
 	assert.EqualError(t, result.Err, "failed to save login details")
 }
