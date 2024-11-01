0234850a1ec471592745a0b932215175e5825148
Fred Hatfull <fhats@stripe.com>
2024-11-01 16:42:44 -0700

    makes formatting + lint happy

--- pkg/cmd/resource/alias.go
+++ pkg/cmd/resource/alias.go
@@ -11,6 +11,7 @@ var aliasedCmds = map[string]string{
 	"line_item": "invoice_line_item",
 }
 
+// GetCmdAlias retrieves the alias for a given resource, if one is present; otherwise returns ""
 func GetCmdAlias(principle string) string {
 	alias, ok := aliasedCmds[principle]
 	if !ok {
@@ -19,13 +20,16 @@ func GetCmdAlias(principle string) string {
 	return alias
 }
 
+// GetAliases retrieves the entire alias map, useful for testing
 func GetAliases() map[string]string {
 	return aliasedCmds
 }
 
+// HideAliasedCommands performs the post-processing on the command tree to hide
+// resources that have an alias
 func HideAliasedCommands(rootCmd *cobra.Command) {
 	for _, cmd := range rootCmd.Commands() {
-		for principle, _ := range aliasedCmds {
+		for principle := range aliasedCmds {
 			formattedPrinciple := GetResourceCmdName(principle)
 			if cmd.Use == formattedPrinciple {
 				cmd.Hidden = true
