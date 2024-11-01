9215691361754019c7764f382705a2e8baf5f7df
Fred Hatfull <fhats@stripe.com>
2024-11-01 16:26:18 -0700

    Adds tests to ensure aliases are hidden in the resource list and that aliased and principle resources hit the same underlying APIs

--- pkg/cmd/resources_test.go
+++ pkg/cmd/resources_test.go
@@ -1,9 +1,16 @@
 package cmd
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"net/http/httptest"
+	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stripe/stripe-cli/pkg/cmd/resource"
 
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
@@ -19,6 +26,34 @@ func TestResources(t *testing.T) {
 	output, err := executeCommand(rootCmd, "resources")
 
 	require.Contains(t, output, "Available commands:")
+	require.NoError(t, err)
+}
+
+func TestResourcesListAliasedName(t *testing.T) {
+	output, err := executeCommand(rootCmd, "resources")
+	require.NoError(t, err)
+
+	assert.Contains(t, output, "Available commands:")
+
+	aliases := resource.GetAliases()
+	for principle, alias := range aliases {
+		aliasRegexp := fmt.Sprintf("\n\\s+%s(s?)\\s+\n", resource.GetResourceCmdName(alias))
+		principleRegexp := fmt.Sprintf("\n\\s+%s(s?)\\s+\n", resource.GetResourceCmdName(principle))
+		assert.Regexp(t, regexp.MustCompile(aliasRegexp), output)
+		assert.NotRegexp(t, regexp.MustCompile(principleRegexp), output)
+	}
+}
+
+func TestAliasedResourcesCallPrincipleAPI(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.URL.Path, "/v1/invoices/in_123/lines")
+	}))
+	defer ts.Close()
+
+	apiBase := fmt.Sprintf("--api-base=%s", ts.URL)
+	_, err := executeCommand(rootCmd, apiBase, "invoice_line_items", "list", "in_123")
+	require.NoError(t, err)
+	_, err = executeCommand(rootCmd, apiBase, "line_items", "list", "in_123")
 	require.NoError(t, err)
 }
 
