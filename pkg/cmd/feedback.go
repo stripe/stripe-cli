5891986c9d5c19279aabe56cb55a209efb2f3f76
Charlie Cruzan <charliecruzan@stripe.com>
2025-03-07 11:56:03 -1000

    fix: remove broken link

--- pkg/cmd/feedback.go
+++ pkg/cmd/feedback.go
@@ -2,10 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"runtime"
-
 	"github.com/spf13/cobra"
-
 	"github.com/stripe/stripe-cli/pkg/validators"
 )
 
@@ -20,8 +17,6 @@ func newFeedbackdCmd() *feedbackCmd {
 			Args:  validators.NoArgs,
 			Short: "Provide us with feedback on the CLI",
 			Run: func(cmd *cobra.Command, args []string) {
-				os := getOS()
-				url := fmt.Sprintf("https://stripe.com/docs/dev-tools-csat%s&devTool=cli", os)
 
 				output := `
      _        _
@@ -34,24 +29,10 @@ func newFeedbackdCmd() *feedbackCmd {
 We'd love to know what you think of the CLI:
 
 * Report bugs or issues on GitHub: https://github.com/stripe/stripe-cli/issues
-* Leave us feedback on how you're using it or features you'd like to see: %s
 				`
 
-				fmt.Println(fmt.Sprintf(output, url))
+				fmt.Println(fmt.Sprintf(output))
 			},
 		},
-	}
-}
-
-func getOS() string {
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		return "?os=Mac"
-	case "linux":
-		return "?os=Linux"
-	case "windows":
-		return "?os=Windows"
-	default:
-		return ""
 	}
 }
