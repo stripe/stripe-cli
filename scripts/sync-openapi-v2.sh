c6f23675cda463c688f5216a89351eb7dff19d54
Tomas Mattia <tomas@stripe.com>
2025-07-01 16:35:23 -0700

    Bump API version to 2025-06-30.basil

--- scripts/sync-openapi-v2.sh
+++ scripts/sync-openapi-v2.sh
@@ -12,7 +12,7 @@ SHA=$(git rev-parse HEAD)
 
 echo "⏳ Retrieving v2 openapi spec..."
 
-./scripts/api-services/apiv2 apispecdump --version="2025-04-30.basil" --format OPENAPI_JSON --variant CLI --out-file spec3.v2.sdk.json
+./scripts/api-services/apiv2 apispecdump --version="2025-06-30.basil" --format OPENAPI_JSON --variant CLI --out-file spec3.v2.sdk.json
 
 popd
 
