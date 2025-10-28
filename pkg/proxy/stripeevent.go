cfe15c6733f6e23a31f947eb57fb85b4ef5752e1
etsai2-stripe <157071424+etsai2-stripe@users.noreply.github.com>
2025-10-28 11:46:16 -0700

    support logged in account id in the listening event URL (#1380)
    * support logged in account id in the listening event URL
    
    * fix tests
    
    * fix extra slash
    
    ---------
    
    Co-authored-by: etsai-stripe <88805634+etsai-stripe@users.noreply.github.com>

--- pkg/proxy/stripeevent.go
+++ pkg/proxy/stripeevent.go
@@ -6,16 +6,17 @@ import "fmt"
 // we define RequestData as an interface for backwards compatibility
 // Request will hold the deserialized request data
 type StripeEvent struct {
-	Account         string                 `json:"account"`
-	APIVersion      string                 `json:"api_version"`
-	Created         int                    `json:"created"`
-	Data            map[string]interface{} `json:"data"`
-	ID              string                 `json:"id"`
-	Livemode        bool                   `json:"livemode"`
-	PendingWebhooks int                    `json:"pending_webhooks"`
-	Type            string                 `json:"type"`
-	RequestData     interface{}            `json:"request"`
-	Request         StripeRequest
+	Account           string                 `json:"account"`
+	APIVersion        string                 `json:"api_version"`
+	Created           int                    `json:"created"`
+	Data              map[string]interface{} `json:"data"`
+	ID                string                 `json:"id"`
+	Livemode          bool                   `json:"livemode"`
+	PendingWebhooks   int                    `json:"pending_webhooks"`
+	Type              string                 `json:"type"`
+	RequestData       interface{}            `json:"request"`
+	Request           StripeRequest
+	LoggedInAccountID string
 }
 
 // StripeRequest is a representation of the Request field in a Stripe `event` object
@@ -31,25 +32,27 @@ func (e *StripeEvent) IsConnect() bool {
 
 // URLForEventID builds a full URL from a StripeEvent ID.
 func (e *StripeEvent) URLForEventID() string {
-	return fmt.Sprintf("%s/events/%s", BaseDashboardURL(e.Livemode, e.Account), e.ID)
+	return fmt.Sprintf("%s/events/%s", BaseDashboardURL(e.Livemode, e.Account, e.LoggedInAccountID), e.ID)
 }
 
 // URLForEventType builds a full URL from a StripeEvent Type.
 func (e *StripeEvent) URLForEventType() string {
-	return fmt.Sprintf("%s/events?type=%s", BaseDashboardURL(e.Livemode, e.Account), e.Type)
+	return fmt.Sprintf("%s/events?type=%s", BaseDashboardURL(e.Livemode, e.Account, e.LoggedInAccountID), e.Type)
 }
 
 // BaseDashboardURL constructs a dashboard URL with (optionally) the test and account values appended
-func BaseDashboardURL(livemode bool, account string) string {
+func BaseDashboardURL(livemode bool, connectedAccountID, loggedInAccountID string) string {
 	maybeTest := ""
 	if !livemode {
 		maybeTest = "/test"
 	}
 
-	maybeAccount := ""
-	if account != "" {
-		maybeAccount = fmt.Sprintf("/%s", account)
+	account := ""
+	if connectedAccountID != "" {
+		account = fmt.Sprintf("/%s", connectedAccountID)
+	} else if loggedInAccountID != "" {
+		account = fmt.Sprintf("/%s", loggedInAccountID)
 	}
 
-	return fmt.Sprintf("https://dashboard.stripe.com%s%s", maybeAccount, maybeTest)
+	return fmt.Sprintf("https://dashboard.stripe.com%s%s", account, maybeTest)
 }
