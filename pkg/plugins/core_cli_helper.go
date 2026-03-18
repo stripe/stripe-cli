fe9244397851d6c41b40a0f7d052a218c93033dd
Vicky Zhang <vzhang@stripe.com>
2026-03-18 16:27:02 -0700

    empty line

--- pkg/plugins/core_cli_helper.go
+++ pkg/plugins/core_cli_helper.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/99designs/keyring"
+
 	"github.com/stripe/stripe-cli/pkg/config"
 	"github.com/stripe/stripe-cli/pkg/plugins/proto"
 	"github.com/stripe/stripe-cli/pkg/stripe"
