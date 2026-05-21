5fd00bbddce8f088d00358ab8a91bb5edf2310de
Yahan Xing <yahanxing@stripe.com>
2026-05-21 10:48:50 -0700

    wip

---
+++ pkg/cmd/root_expired_key_test.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAPIKeyExpiredMessage_DefaultProfile(t *testing.T) {
+	msg := apiKeyExpiredMessage("default")
+	require.Contains(t, msg, "default profile")
+	require.Contains(t, msg, "`stripe login`")
+	require.NotContains(t, msg, "--project-name")
+}
+
+func TestAPIKeyExpiredMessage_NamedProfile(t *testing.T) {
+	msg := apiKeyExpiredMessage("work")
+	require.Contains(t, msg, `"work"`)
+	require.Contains(t, msg, "--project-name=work")
+}
