b50d10996c16150ce88ccf487300110f297b9e88
adelechen <adelechen@stripe.com>
2026-09-18 19:26:59 +0000

    lint

--- pkg/cmd/agent_test.go
+++ pkg/cmd/agent_test.go
@@ -371,7 +371,7 @@ type agentCall struct {
 }
 
 func TestAgentSetupAutoInstallsForCallingAgent(t *testing.T) {
-	calling_agents := []struct {
+	callingAgents := []struct {
 		name         string
 		displayName  string
 		makeProvider func(record agentsetup.RunCommandFunc) agentsetup.Provider
@@ -391,7 +391,7 @@ func TestAgentSetupAutoInstallsForCallingAgent(t *testing.T) {
 		},
 	}
 
-	for _, agent := range calling_agents {
+	for _, agent := range callingAgents {
 		t.Run(agent.name, func(t *testing.T) {
 			var calls []agentCall
 			record := func(_ context.Context, name string, args ...string) error {
